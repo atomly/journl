93fc004126494d398c11c7af66c7be556d7f5ece
Robert Molina <rmolinamir@gmail.com>
2026-02-27 14:56:26 -0500

    fix(web): update ui and a11y

--- apps/web/src/app/(app)/@header/_components/fixed-header.tsx
+++ apps/web/src/app/(app)/@header/_components/fixed-header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import type React from "react";
 import { useEffect, useRef, useState } from "react";
 import { useIsMobile } from "~/hooks/use-mobile";
 import { cn } from "~/lib/cn";

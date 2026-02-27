e91cef8194d6e691e3397d528a35088e6b74b734
Robert Molina <rmolinamir@gmail.com>
2026-02-27 14:58:27 -0500

    chore: cleanup

--- apps/web/src/app/(app)/@header/_components/fixed-header.tsx
+++ apps/web/src/app/(app)/@header/_components/fixed-header.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import type React from "react";
 import { useEffect, useRef, useState } from "react";
 import { useIsMobile } from "~/hooks/use-mobile";
 import { cn } from "~/lib/cn";
